cab972461736b687d02b8812693b308e90b772c9
Abhaymehta1424 <114793909+Abhaymehta1424@users.noreply.github.com>
2022-10-07 09:11:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+#print( "New to python",end="  " )
+#print("a")
+#print("/negligible")
+#int='what is this'
+#print("int")
+#a=43
+#b=4344
+#print(a+b)
+#c="234"
+#d="324"
+#print(c+d)
+#print("enter your number")
+#inpnum = input()
+
+#print("you entered", (inpnum))
+#print("enter your first number")
+#n1 = input()
+#print("enter your second number")
+#n2 = input()
+#print("sum of these two numbers is",int(n1) + int(n2))
+#str="my name is abhay"
+#print(len(str))
+#print(str[0:13])
+#print(str[4:17:2])
+#print(str[-4:-2]) piche se no. negative
+
+#print(str.isalnum()) if space is removed from the string sentence it will come to be true
+#print(str.) sare int likhne se aa jate hai wether to captalize or to do in lower case
+#print(str.replace("abhay","mehta"))
+
+
